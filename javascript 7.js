a125b61ec27222f7f808f3385b14775d01d150b3
odunola26 <62163826+odunola26@users.noreply.github.com>
2021-05-21 00:44:54 -0700

    Add files via upload
    find the attached

---
+++ javascript 7.js
@@ -0,0 +1,41 @@
+let Array=[];
+function checkYuGiOh (n)
+{for (i=1; i<=n; i++){
+ 
+ if(i%2==0 && i%3==0 && i%5==0){
+ Array.push("yu-gi-oh");
+}
+
+ else if (i%3==0 && i%5==0){
+   Array.push("gi-oh");
+ }
+
+ else if( i%2 ==0 && i%5==0){
+   Array.push("yu-oh");
+ }
+
+   else if (i%2==0 && i%3==0){
+    Array.push("yu-gi");
+   }
+
+    else if (i%5==0){
+     Array.push("oh");
+   } 
+
+     else if (i%3==0){
+       Array.push ("gi");}
+   
+    else if (i%2==0){
+     Array.push("yu");
+   }
+ 
+    else{Array.push(i)
+   } 
+}
+       console.log(Array)
+      return  Array
+      
+}
+checkYuGiOh(10);
+
+checkYuGiOh("5");
